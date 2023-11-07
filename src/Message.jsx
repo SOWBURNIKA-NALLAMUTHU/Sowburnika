e5c81b5b42e990f15aaa7b996e49469726627193
SOWBURNIKA-NALLAMUTHU <727722euit180@skcet.ac.in>
2023-11-07 11:34:44 +0530

    Initial Commit

---
+++ src/Message.jsx
@@ -0,0 +1,14 @@
+//JSX- JavaScript XML
+
+//addTwoNumbers camelbasic
+function Message() //classical basic (react follows this)
+{
+   return(
+    <div>
+        <h1>From Message.jsx</h1>
+        <p>Hello World</p>
+    </div>
+   );
+}
+
+export {Message};
